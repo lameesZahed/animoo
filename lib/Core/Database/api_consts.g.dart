715f4640036da49aebe5a0d121f3c60a4fa9ca26
lameesZahed <lameeszahed17@gmail.com>
2026-08-20 00:20:51 +0300

    Initial commit

---
+++ lib/Core/Database/api_consts.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_consts.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// generated_from: .env
+final class _ApiConsts {
+  static const String baseUrl = 'http://192.168.117.1:8000';
+
+  static const String signUpEndpoint = '/api/signup';
+
+  static const String logInEndpoint = '/api/login';
+}
