789f91fe2812a4e441649507b90fe01c38a97d3f
lameesZahed <lameeszahed17@gmail.com>
2026-08-20 19:11:11 +0300

    Initial commit

--- lib/Core/Database/api_consumer.dart
+++ lib/Core/Database/api_consumer.dart
@@ -1,7 +1,23 @@
 abstract class ApiConsumer{
-  get();
-  post();
-  delete();
-  patch();
-  put();
+  const ApiConsumer();
+  Future <dynamic>get({
+    required String url,
+    Map<String, dynamic>? queryParameters,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? body,
+});
+  Future <dynamic>post({
+    required String url,
+    Map<String, dynamic>? queryParameters,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? body,
+});
+  Future <dynamic>delete({
+    required String url,
+    Map<String, dynamic>? queryParameters,
+    Map<String, dynamic>? headers,
+    Map<String, dynamic>? body,
+});
+  // patch();
+  // put();
 }
---
+++ lib/Core/Database/dio_service.dart
@@ -0,0 +1,23 @@
+import 'api_consumer.dart';
+
+class DioService implements ApiConsumer {
+  @override
+  Future delete({required String url, Map<String, dynamic>? queryParameters, Map<String, dynamic>? headers, Map<String, dynamic>? body}) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future get({required String url, Map<String, dynamic>? queryParameters, Map<String, dynamic>? headers, Map<String, dynamic>? body}) {
+    // TODO: implement get
+    throw UnimplementedError();
+  }
+
+  @override
+  Future post({required String url, Map<String, dynamic>? queryParameters, Map<String, dynamic>? headers, Map<String, dynamic>? body}) {
+    // TODO: implement post
+    throw UnimplementedError();
+  }
+  // Dio dio ;
+}
+
